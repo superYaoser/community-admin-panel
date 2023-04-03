a11944fb4dc887e8b44038ddfe7e2398cdb7469f
Yaoser <yao@yao.cn>
2023-04-03 09:28:29 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store)
+
+// @ts-ignore
+app.use(ElementPlus)
+
+app.use(router).mount('#app')
