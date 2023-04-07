dcaae4634763153771fcb2209b3e79d80a9b6e46
Yaoser <yao@yao.cn>
2023-04-07 16:03:34 +0800

    完善了header的样式

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="header">
-  <div class="left">
+  <div class="left" style="padding-left: 10px">
     <span style="font-size: 20px">文章管理</span>
   </div>
-    <div class="right"></div>
+    <div class="right">
+      <div>
+        <el-avatar
+            size="default"
+            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+        />
+      </div>
+      <el-dropdown @command="handleCommand">
+    <span class="el-dropdown-link">
+      测试用户<el-icon class="el-icon--right"><arrow-down /></el-icon>
+    </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item command="a"><el-icon><User /></el-icon>个人信息</el-dropdown-item>
+            <el-dropdown-item command="b"><el-icon><EditPen /></el-icon>修改密码</el-dropdown-item>
+            <el-dropdown-item command="c"><el-icon><CircleCloseFilled /></el-icon>退出系统</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-  name: "Header"
+  name: "Header",
+  setup(){
+    const handleCommand = (command: string | number | object) => {
+      console.log(`click on item ${command}`)
+    }
+    return{
+      handleCommand
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
 #header{
   padding: 0 20px;
   margin-left: 220px;
@@ -27,7 +60,17 @@ export default {
 }
 .right{
   height: 20px;
-  width: 70px;
-  background: #b71b1b;
+  width: 150px;
+  margin-right: 30px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  div{
+    flex: 1;
+    text-align: center;
+    &:not{
+      border-right: 1px solid var(--el-border-color);
+    }
+  }
 }
 </style>
--- src/components/side/Side.vue
+++ src/components/side/Side.vue
@@ -12,7 +12,7 @@
               active-text-color="#ffd04b"
               background-color="#545c64"
               class="el-menu-vertical-demo"
-              default-active="2"
+              default-active="1"
               text-color="#fff"
               @open="handleOpen"
               @close="handleClose"
